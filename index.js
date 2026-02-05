29631ea6867177c8363471ad5f02a81ac24df50f
Keegan <keegantournay@gmail.com>
2026-02-05 16:20:18 -0500

    chore: create landmark class

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+class Landmark {
+    constructor(title, description, image, coordinates) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.coordinates = coordinates;
+    }
+}
+
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 43.2375, lng: -79.8341},
