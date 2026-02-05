2c01d5149549a4b6f553047986aafaef03e2c8ce
Keegan <keegantournay@gmail.com>
2026-02-05 16:08:20 -0500

    feat: add Google Maps display

---
+++ index.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 43.2375, lng: -79.8341},
+        zoom: 11,
+        mapId: "806ec8f84b08b4552faf418c"
+    });
+}
